276c70ae71e45226f2df611ffd2057156d800000
unknown <jesseelijah.nance@gmail.com>
2023-02-08 17:04:39 -0500

    added files

---
+++ server/schema/resolvers.js
@@ -0,0 +1,18 @@
+const { User } = require('../models');
+
+const resolver = {
+    Query: {
+        async getSingleUser(_parent, args, context) {
+            const { user } = context;
+            const foundUser = await User.findOne({
+                $or: [
+                    { _id: user ? user._id : args.id }, 
+                    { username: args.username }],
+            });
+            return foundUser;
+        },
+    }
+
+};
+
+module.exports = resolvers;
